4111f8ef6eb2251aa69415bc43495de8e9e27b5a
Josh Lasdin <joshlasdindesign@gmail.com>
2012-02-13 14:25:22 -0500

    Split files out for submodule

--- lib/inc/meta-footer.php
+++
@@ -1,6 +0,0 @@
-<script src="/lib/js/min/scripts-min.js"></script>
-
-<!--[if lt IE 7 ]>
-	<script src="//ajax.googleapis.com/ajax/libs/chrome-frame/1.0.3/CFInstall.min.js"></script>
-	<script>window.attachEvent('onload',function(){CFInstall.check({mode:'overlay'})})</script>
-<![endif]-->
