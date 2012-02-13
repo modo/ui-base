8fa39c5aa0f71240dec498c19aa06a172bbb1b12
Travis Lehman <trav.lehman@gmail.com>
2012-02-13 10:53:59 -0500

    Added files

---
+++ lib/inc/meta-head.php
@@ -0,0 +1,7 @@
+<link rel="stylesheet" href="/lib/css/min/styles.css" />
+<link rel="stylesheet" href="/lib/css/min/responsive.css" />
+
+<link href="//get.pictos.cc/fonts/734/2" rel="stylesheet" type="text/css">
+
+<script src="http://use.typekit.com/rsm7gxu.js"></script>
+<script src="/lib/js/min/preload-min.js"></script>
